e4ae868281b6eda7fa4f273d0a51963255b6f516
Your Name <you@example.com>
2024-09-26 10:22:59 +0300

    Izdariju no 1. līdz 4., izveidoju index un blog php failus un sarakstiju visu kas bija rakstits lidz 4 punktam

---
+++ web/images/blog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tiešsaistes dienasgrāmata</title>
+</head>
+<body>
+    <div>
+    <h1>Biogrāfija</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti dolorum voluptate quam nesciunt. Distinctio iste itaque corporis eum asperiores ipsum sint molestias, magnam illo amet fugit nemo autem at vero. Sint quos hic, eos et saepe suscipit? Obcaecati explicabo iure voluptas voluptatibus, necessitatibus eum minima itaque sint dolore veritatis, placeat ullam aliquid perferendis fugit ex. Vero vel beatae odit? Nam dignissimos enim soluta doloribus debitis est cumque saepe maiores sequi animi! Unde beatae vel aperiam reprehenderit quo qui ducimus repudiandae totam, explicabo quidem dolorum ipsum molestiae consequatur mollitia consectetur earum nesciunt tenetur incidunt nisi enim iusto? Consectetur consequuntur dignissimos aliquam!</p>
+    <h2>Kāpēc es nolēmu kļūt par programmētaju?</h2>
+    <ol>
+        <li>Lorem ipsum dolor sit amet.</li>
+        <li>Lorem ipsum dolor sit.</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+        <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odit.</li>
+        <li>Lorem, ipsum dolor.</li>
+    </ol>
+    </div>
+    <button type="button">Blogs</button>
+    <input type="button" value="Blogs">
+    
+</body>
+</html>
---
+++ web/images/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Piesakies webināram!</title>
+</head>
+<body>
+    <form action="" method="post">
+        <select name="vards" id="vards">
+        <option value=" "> </option>
+        <option value="Beginner">Beginner</option>
+        <option value="Intermediate">Intermediate</option>
+        <option value="Advanced">Advanced</option>
+    </select>
+        <br>
+        <p>Vai vēlies saņemt info par citiem mūsu organizētajiem pasākumiem?</p>
+
+    <input type="radio" name="info" id="info">
+        <label for="info">Jā</label>
+        <input type="radio" name="info" id="info">
+        <label for="info">Nē</label>
+
+    </form>
+    <br>
+    <button type="submit">iesniegts</button>
+    <br>
+    <br>
+    <input type="button" value="blogs">
+
+ 
+    
+
+    
+
+
+
+
+</body>
+</html>
